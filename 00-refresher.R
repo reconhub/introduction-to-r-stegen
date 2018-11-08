b42970f208eed098baa0360571c6a1436d2dd37d
Zhian N. Kamvar <zkamvar@gmail.com>
2018-11-08 12:12:57 +0000

    first step at adding refresher R script

---
+++ 00-refresher.R
@@ -0,0 +1,47 @@
+#' ---
+#' title: 'Refresher for R'
+#' author: Zhian N. Kamvar
+#' date: 2018-11-08
+#' ---
+#' 
+#' # Installing R and Rstudio
+#' 
+#' - Installing R: go here and follow the instructions <https://cloud.r-project.org/>
+#'
+#' - Instaling RStudio: go here and follwo the instructions <https://www.rstudio.com/products/rstudio/download/#download>
+#'
+#' # Navigating RStudio
+#'
+#' # Entering commands into R
+
+# R is a calculator
+sqrt(3^2 + 4^2) # Pythagorean theorem a^2 + b^2 = c^2
+
+# R can test for equality
+
+5 == sqrt(3^2 + 4^2) # == means "is equal to"
+
+# R can do several calculations at once
+
+1:10
+1:10 + 2
+
+
+#' # Creating and changing variables (also known as objects)
+
+my_numbers <- 1:10
+my_numbers
+my_numbers + 2
+my_numbers <- "whee"
+my_numbers
+
+#' # Data types
+
+#' # Importing data
+
+#' # Installing and using packages
+
+#' # Getting help
+
+
+#' 
